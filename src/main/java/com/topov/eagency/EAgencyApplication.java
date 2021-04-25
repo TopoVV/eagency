ff3ab140ff14fb3b16788d4b46beeae5ff063cfd
Vladislav Topov <vladtopov2001@gmail.com>
2021-04-26 01:18:57 +0300

    Initial package structure

---
+++ src/main/java/com/topov/eagency/EAgencyApplication.java
@@ -0,0 +1,13 @@
+package com.topov.eagency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EAgencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EAgencyApplication.class, args);
+    }
+
+}
